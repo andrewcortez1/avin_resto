56ba22a81f84d4bf9d0b9e70f5c848bf883920ed
andrewcortez13 <andrewgeraldo13@gmail.com>
2021-06-12 16:36:22 +0700

    Update Baru

--- Restaurant Web v2/create_new_menu.php
+++ Restaurant Web v2/create_new_menu.php
@@ -1,22 +1,29 @@
 <?php
+	include "background.php";
 	include "connection.php";
-
+	echo ' <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">';
 	$count = mysqli_query($resto, "SELECT *FROM menu");
 	$counter =0;
 	while ($row = mysqli_fetch_array($count)){
 		$counter++;
 	}
-	
-	echo "Total Menu Count : " .$counter . "<br>";
+	echo "<style>
+	h3{
+		color : aqua;
+	}
+	</style>";
+
+	echo "<div class='d-flex justify-content-center'>";
 	echo "<form method = 'post' action = 'input_menu.php'>";
-	echo "Input no Makanan ";
+	echo "<h3>Total Menu Count : " .$counter . "<br></h3></br>";
+	echo "<h3>Input no Makanan </h1>";
 	echo "<input type = 'text' name = 'no'></br>";
-	echo "Input nama Makanan: ";
+	echo "<h3>Input nama Makanan: </h1>";
 	echo "<input type = 'text' name = 'nama'></br>";
-	echo "Input harga makanan: ";
+	echo "<h3>Input harga makanan: </h1>" ;
 	echo "<input type = 'text' name = 'harga'>";
 	
 	echo "<input type = 'submit' value = 'Submit'> ";
-	echo "</form>";
+	echo "</form></div>";
 
 ?>
